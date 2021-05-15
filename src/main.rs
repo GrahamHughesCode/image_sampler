2736301710bfc8c84063b1108f7d67b93502a5c7
Graham Hughes <end_sub@DESKTOP-1ML78E4.localdomain>
2021-05-15 13:49:47 -0400

    added size selection

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,21 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let file = args[1].clone();
     let img = image::open(file).unwrap();
-    let scaled = img.resize(200, 200, FilterType::Nearest);
+    let mut x_pixels:u32 = 100;
+    let mut y_pixels:u32 = 100;
+    if args.len() >= 3
+    {
+        match args[2].as_str()
+        {
+            "small" => x_pixels = 100,
+            "medium" => x_pixels = 200,
+            "large" => x_pixels = 300,
+            _ => println!("error")
+        } 
+        y_pixels = (img.dimensions().1 as f32 * (x_pixels as f32 /img.dimensions().0 as f32)) as u32;
+    }
+    println!("x: {} y: {}" ,x_pixels,y_pixels);
+    let scaled = img.resize(x_pixels, y_pixels, FilterType::Nearest);
     let img = scaled; 
     let x_size = img.dimensions().0 - 1;
     let mut output = String::new();
